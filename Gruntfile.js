77b82acd5515b6d2afd0d173d7273306bc039335
Matthew Dean <deanmatthew16@gmail.com>
2017-02-18 12:04:23 -0500

    Use jspm/SystemJS as frontend package manager/loader
    - Fewer dependencies: no longer need separate dependencies for Grunt
      tasks for uglify, bablel, and minify.
    - Remove now unnecessary browserify and browserify-shim properties
      from package.json
    - No longer need a dist and staging directory in build folder, now
      all files are copied into build/ directly before being copied to
      the public folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 let fs = require('fs');
+let path = require('path');
 
 module.exports = function(grunt) {
+    let pkg = grunt.file.readJSON('package.json');
+
+    let clientBase = 'app/client/'
+    let build = clientBase + 'build/';
     let finalDist = 'app/server/public/';
 
-    let build = 'app/client/build/';
-    let buildStaging = build + 'staging/';
-    let buildDist = build + 'dist/';
-
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: pkg,
         clean: {
-            testPrep: ['.cache', 'build'],
             buildPrep: [build, finalDist]
         },
         mochaTest: {
@@ -55,39 +55,6 @@ module.exports = function(grunt) {
                 src: 'build/reports/coverage/lcov.merged.info'
             }
         },
-        browserify: {
-            // Enable source maps at the end of the file
-            // options: {
-            //     browserifyOptions: {
-            //         debug: true
-            //     }
-            // },
-            app: {
-                src: 'app/client/app.module.js',
-                dest: buildStaging + 'app.browserify.js'
-            }
-        },
-        babel: {
-            options: {
-                presets: ['es2015'],
-                compact: true
-            },
-            app: {
-                files: {
-                    [buildStaging + 'app.babel.js']: buildStaging + 'app.browserify.js'
-                }
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! Grunt Uglify <%= grunt.template.today("yyyy-mm-dd") %> */ ',
-            },
-            app: {
-                files: {
-                    [buildStaging + 'app.min.js']: [buildStaging + 'app.babel.js']
-                }
-            }
-        },
         cssmin: {
             options: {
                 sourceMap: true
@@ -105,39 +72,55 @@ module.exports = function(grunt) {
                 }
             }
         },
+        systemjs: {
+            options: {
+                sfx: true,
+                configFile: clientBase + 'config.js',
+                // minify: true, // uncommenting breaks build
+                sourceMaps: true,
+                format: 'cjs',
+                baseURL: clientBase
+            },
+            dist: {
+                files: [{
+                    src: 'app.module.js',
+                    dest: path.join(build, 'scripts/app.min.js')
+                }]
+            }
+        },
         copy: {
             fonts: {
                 cwd: 'node_modules/bootstrap/dist/fonts/',
                 src: '*',
-                dest: buildDist + 'fonts',
+                dest: build + 'fonts',
                 expand: true
             },
             rawAssets: {
                 cwd: 'app/client/_assets/raw',
                 src: '*',
-                dest: buildDist + 'assets',
+                dest: build + 'assets',
                 expand: true
             },
             scripts: {
-                cwd: buildStaging,
+                cwd: build,
                 src: 'app.min.js',
-                dest: buildDist + 'scripts',
+                dest: finalDist + 'scripts',
                 expand: true
             },
             style: {
-                cwd: buildDist + 'style',
+                cwd: build + 'style',
                 src: ['*.min.css', '*.min.css.map'],
                 dest: finalDist + 'style',
                 expand: true
             },
             views: {
-                cwd: buildDist + 'views',
+                cwd: build + 'views',
                 src: '**',
                 dest: finalDist + 'views',
                 expand: true
             },
             dist: {
-                cwd: buildDist,
+                cwd: build,
                 src: '**', // copy all files and subdirectories
                 dest: finalDist,
                 expand: true
@@ -146,7 +129,7 @@ module.exports = function(grunt) {
         watch: {
             js: {
                 files: ['app/client/*.js', 'app/client/!(build)/**/*.js'],
-                tasks: ['browserify', 'babel', 'uglify', 'copy:scripts']
+                tasks: ['systemjs', 'copy:scripts']
             },
             css: {
                 files: ['./app/client/_assets/style/*.css'],
@@ -172,7 +155,7 @@ module.exports = function(grunt) {
                 `app/client/_assets/style/${css}.css`,
                 'node_modules/bootstrap/dist/css/bootstrap.css'
             ],
-            dest: buildDist + `style/${css}.min.css`
+            dest: build + `style/${css}.min.css`
         });
     });
     grunt.config(cssminProp, files);
@@ -199,13 +182,13 @@ module.exports = function(grunt) {
     // app/server/src/views
 
     let srcDir = 'app/server/src/views/';
-    let outDir = buildDist + 'views/';
+    let outDir = build + 'views/';
     let filesMap = {};
 
     // Data to be passed to every template
     let data = {
         year: new Date().getFullYear(),
-        appName: grunt.config('pkg').name
+        appName: pkg.name
     };
 
     // All views that can't be rendered statically or shouldn't be rendered
@@ -227,20 +210,18 @@ module.exports = function(grunt) {
     grunt.config('pug.compile.options.data', data);
 
     var tasks = [
-        'babel',
-        'browserify',
         'contrib-clean',
         'contrib-copy',
         'contrib-cssmin',
         'contrib-jshint',
-        'contrib-uglify',
         'contrib-pug',
         'contrib-watch',
         'coveralls',
         'karma',
         'lcov-merge',
         'mocha-test',
-        'mocha-istanbul'
+        'mocha-istanbul',
+        'systemjs-builder'
     ];
 
     for (var i = 0; i < tasks.length; i++) {
@@ -251,5 +232,5 @@ module.exports = function(grunt) {
     grunt.registerTask('test', ['mochaTest', 'karma']);
     grunt.registerTask('testCoverage', ['clean:testPrep', 'mocha_istanbul', 'karma']);
     grunt.registerTask('uploadCoverage', ['lcovMerge', 'coveralls']);
-    grunt.registerTask('build', ['clean:buildPrep', 'browserify', 'babel', 'uglify', 'cssmin', 'pug', 'copy']);
+    grunt.registerTask('build', ['clean:buildPrep', 'systemjs', 'cssmin', 'pug', 'copy']);
 };
