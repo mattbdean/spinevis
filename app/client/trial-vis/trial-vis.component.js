061446607a89e419918a9276391d26918e3f9779
Matthew Dean <deanmatthew16@gmail.com>
2017-02-23 16:57:44 -0500

    Add proof-of-concept timeline render

--- app/client/trial-vis/trial-vis.component.js
+++ app/client/trial-vis/trial-vis.component.js
@@ -1,6 +1,9 @@
 let moment = require('moment');
 require('moment-duration-format');
 
+// TODO Use JSPM to require plotly. Currently Plotly is added through a <script>
+// let Plotly = require('plotly/plotly.js');
+
 let ctrlDef = ['$http', '$window', function TrialVisController($http, $window) {
     let $ctrl = this;
 
@@ -32,6 +35,38 @@ let ctrlDef = ['$http', '$window', function TrialVisController($http, $window) {
             Length: calculateDifference($ctrl.trialMeta.start_time, $ctrl.trialMeta.end_time)
         };
         console.log($ctrl.trialMeta);
+    });
+
+    $http.get('/api/v1/trial/' + $ctrl.trialId + '/timeline').then(function(result) {
+        $ctrl.timelineData = result.data.data;
+
+        // Create timeline outline
+        let trace = {
+            x: [],
+            y: [],
+            type: 'line'
+        };
+
+        // Simple layout data
+        let layout = {
+            yaxis: {
+                title: 'Fluorescence'
+            },
+            xaxis: {
+                title: 'Time'
+            }
+        };
+
+        let start = Date.now();
+        // Fill in the trace data with timeline data
+        for (let i = 0; i < $ctrl.timelineData.length; i++) {
+            trace.x[i] = $ctrl.timelineData[i].absTime;
+            trace.y[i] = $ctrl.timelineData[i].globalF;
+        }
+
+        Plotly.newPlot('plot-timeline', [trace], layout);
+        let delta = Date.now() - start;
+        console.log('Created timeline traces and plotted in ' + (delta / 1000) + ' seconds');
     });
 }];
 
