fac7f5a26767aef23e8e6f491b6337e6af1b7fae
Matthew Dean <deanmatthew16@gmail.com>
2017-03-20 17:56:33 -0400

    List available endpoints before starting

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
+let listEndpoints = require('express-list-endpoints');
+let _ = require('lodash');
+
 // Catch unhandled Promises
 process.on('unhandledRejection', function(reason, p) {
     console.error("Unhandled Promise rejection: ");
@@ -11,8 +14,14 @@ process.on('unhandledRejection', function(reason, p) {
 const port = process.env.PORT || 8080;
 
 require('./app/server/src/server.js')().then(function(app) {
+    console.log('Available endpoints:\n');
+    let endpoints = _.sortBy(listEndpoints(app), (e) => e.path);
+    for (let e of endpoints) {
+        console.log(`  ${_.join(e.methods, ', ')} ${e.path}`);
+    }
+
     app.listen(port);
-    console.log('Magic is happening on port ' + port);
+    console.log('\nMagic is happening on port ' + port);
 }).catch(function(err) {
     throw err;
 });
