b36fd61bce96f3feb949043be3f96b06349343ed
Matthew Dean <deanmatthew16@gmail.com>
2017-05-08 15:16:10 -0400

    Fixed an issue caused by downsampling at uneven resolutions

--- app/client/src/timeline/downsampler.service.js
+++ app/client/src/timeline/downsampler.service.js
@@ -50,7 +50,7 @@ const serviceDef = ['session', function DownsamplerService(session) {
                 // the downsampled array.
 
                 // 50% res = chunk size of 2, 25% res = chunk size of 4, etc.
-                const chunkSize = RESOLUTION_FULL / resolution;
+                const chunkSize = Math.round(RESOLUTION_FULL / resolution);
                 const chunks = _.chunk(fullRes, chunkSize);
 
                 for (let i = 0; i < chunks.length; i++) {
