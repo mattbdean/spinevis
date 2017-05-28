d69eedf14b1a8ce26021b90081b20660b877fbb2
Matthew Dean <deanmatthew16@gmail.com>
2017-05-28 15:13:58 -0400

    Write a simple endpoint at /api/v1/animal to return all animal names

---
+++ app/server/src/routes/api/util.js
@@ -0,0 +1,67 @@
+const _ = require('lodash');
+
+const queries = require('../../queries');
+const responses = require('./responses');
+
+/**
+ * Runs a query and sends the result as JSON to the response. Takes input
+ * Parameters and if all are valid, passes values to queryFn
+ *
+ * @param  {[]}        parameters An array of Parameters
+ * @param  {function}  queryFn    Query function to execute
+ * @param  {object}    res        Express Response object
+ * @param  {function}  next       Express 'next' function
+ */
+module.exports.runQuery = function(parameters, queryFn, res, next, paginated = false, contracts = []) {
+    // If any parameter is invalid, reject
+    for (const p of parameters) {
+        if (p.valid === false) {
+            return next(responses.errorObj(p.error));
+        }
+    }
+
+    for (const contract of contracts) {
+        contract.check(parameters);
+        if (contract.valid === false) {
+            return next(responses.errorObj(contract.error));
+        }
+    }
+
+    // Call queryFn with the parameter values
+    queryFn.apply(null, _.map(parameters, (i) => i.value))
+        .then(function(result) {
+            // Choose the correct type of response, whether that be a standard
+            // success or a paginated success object
+            let response;
+            if (paginated) {
+                // Identify values of start and limit
+                const start = _.find(parameters, (p) => p.name === 'start').value;
+                const limit = _.find(parameters, (p) => p.name === 'limit').value;
+                response = responses.paginatedSuccess(result, limit, start);
+            } else {
+                response = responses.success(result);
+            }
+            res.json(response);
+        }).catch(function(err) {
+        // Handle errors. If the error has a type property, we know it was
+        // from a function in the queries module.
+        if (err.type) {
+            let status;
+            if (err.type === queries.ERROR_MISSING)
+                status = 404;
+            else if (err.type === queries.ERROR_PAGINATION)
+            // Send 400 Bad Request because the client sent bad data
+                status = 400;
+
+            // Make sure the type was one of the errors already checked for
+            if (status !== undefined) {
+                return next(responses.error(err.message, err.data, status));
+            }
+        }
+
+        // We don't know how to handle this kind of error, send a 500 Internal
+        // Server Error.
+        return next(responses.error());
+    });
+};
+
