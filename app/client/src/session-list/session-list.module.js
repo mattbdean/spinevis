13e18923fba987e47a4a7816077892c22d418610
Matthew Dean <deanmatthew16@gmail.com>
2017-05-28 12:39:05 -0400

    Make session-list depend on angular-material
    This code worked before because the main module depended on ngMaterial and
    session-list didn't. However, if session-list is used independently/copied and
    pasted into another project, the code would fail.

--- app/client/src/session-list/session-list.module.js
+++ app/client/src/session-list/session-list.module.js
@@ -3,7 +3,9 @@ const core = require('../core/core.module.js');
 const sessionListComponent = require('./session-list.component.js');
 const ngInfiniteScroll = require('ng-infinite-scroll');
 
-module.exports = angular.module('sessionList', [core.name, ngInfiniteScroll])
+const ngMaterial = require('angular-material');
+
+module.exports = angular.module('sessionList', [core.name, ngInfiniteScroll, ngMaterial])
     .component('sessionList', sessionListComponent);
 
 require('./session-list.scss');
